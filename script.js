ae2e609c42baf6067865c12afd45c95fc1504334
WALID CHAHIDANE <chahidanwalid37@gmail.com>
2022-09-09 00:15:19 +0100

    Devinette

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-let body=document.body;
-let btn=document.querySelector("button");
-let colors=["red","green","blue","gray"];
+let btn = document.getElementById("btn");
+let input = document.querySelector("input");
+let res=document.getElementById("result");
+var gess=randomBetween(1,10);
+  alert(gess);
 
-function changeBgcolor(){
-  let randomIndex=Math.floor(Math.random()*colors.length);
-  body.style.backgroundColor=colors[randomIndex];
-}
-setInterval(changeBgcolor,3000);
-btn.addEventListener("click",changeBgcolor);
+btn.addEventListener("click", function () {
+  
+  let chance = 3;
+  while (chance > 0) {
+    let nbr = input.value;
+    if (nbr > gess) {
+      alert("plus petit");
+      chance--;
+    } else if (nbr == gess) {
+      alert("bravooo!!!");
+      chance = 0;
+    } else {
+      alert("plus grand");
+      chance--;
+    }
+  }
+});
+function randomBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
