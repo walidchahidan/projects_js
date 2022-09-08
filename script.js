f06acf549d8506524a1c6931fb24b78032db91de
WALID CHAHIDANE <chahidanwalid37@gmail.com>
2022-09-08 17:22:27 +0100

    BMIcalcul

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-let poid=document.getElementById("weight_txt");
-let long=document.getElementById("height_txt");
-let btn=document.querySelector("button");
-let show=document.querySelector("p");
+let poid = document.getElementById("weight_txt");
+let long = document.getElementById("height_txt");
+let btn = document.querySelector("button");
+let show = document.querySelector("p");
 
-function calculer(){
-  let res=(poid.value/(long.value*long.value)).toFixed(2);
-  
+function calculer() {
+  let res = (poid.value / (long.value * long.value)).toFixed(2);
+
   alert(res);
-  if(res>=18.5 && res<=25){
+  poid.value = "";
+  long.value = "";
+  if (res >= 18.5 && res <= 25) {
     // alert("Normal");
-    
-    show.textContent=`${res} you are Normal`;
-  }
-  else if(res>=25 && res<=30){
+
+    show.textContent = `${res} you are Normal`;
+  } else if (res >= 25 && res <= 30) {
     // alert("Surpoids");
-    show.textContent=`${res} you are Superpoids`;
-  }
-  else if(res>=30 && res<=35){
+    show.textContent = `${res} you are Superpoids`;
+  } else if (res >= 30 && res <= 35) {
     // alert("Obésité modérée");
-    show.textContent=`${res} you are Obésité modérée`;
+    show.textContent = `${res} you are Obésité modérée`;
   }
 }
 
-btn.addEventListener("click",calculer);
+btn.addEventListener("click", calculer);
