3f93caed06b1634fc2b7f0875b1671c0f4603673
WALID CHAHIDANE <chahidanwalid37@gmail.com>
2022-09-09 00:49:58 +0100

    Devinette

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
+
 let btn = document.getElementById("btn");
 let input = document.querySelector("input");
 let res=document.getElementById("result");
 var gess=randomBetween(1,10);
   alert(gess);
-
+var chance = 3;
 btn.addEventListener("click", function () {
   
-  let chance = 3;
+  
   while (chance > 0) {
-    let nbr = input.value;
+    var nbr = Number(input.value);
     if (nbr > gess) {
       alert("plus petit");
+      // res.textContent = `plus petit que ${nbr}`;
       chance--;
+      // console.log(chance);
     } else if (nbr == gess) {
       alert("bravooo!!!");
+      // res.textContent = `BRAVOOOOOOOOOOOOO!!!!`;
       chance = 0;
+      // console.log(chance);
     } else {
       alert("plus grand");
+      // res.textContent = `plus grand que ${nbr}`;
       chance--;
+      // console.log(chance);
     }
   }
 });
 function randomBetween(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+   }
