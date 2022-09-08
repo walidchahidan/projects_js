232ad24e8afcb4eba07eb06233fb7b5d42495e78
WALID CHAHIDANE <chahidanwalid37@gmail.com>
2022-09-08 16:30:42 +0100

    BMIcalcul

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let body=document.body;
+let poid=document.getElementById("weight_txt");
+let long=document.getElementById("height_txt");
 let btn=document.querySelector("button");
-let colors=["red","green","blue","gray"];
+let show=document.querySelector("p");
 
-function changeBgcolor(){
-  let randomIndex=Math.floor(Math.random()*colors.length);
-  body.style.backgroundColor=colors[randomIndex];
+function calculer(){
+  let res=(poid.value/(long.value*long.value)).toFixed(2);
+  
+  alert(res);
+  if(res>=18.5 && res<=25){
+    // alert("Normal");
+    
+    show.textContent=`${res} you are Normal`;
+  }
+  else if(res>=25 && res<=30){
+    // alert("Surpoids");
+    show.textContent=`${res} you are Superpoids`;
+  }
+  else if(res>=30 && res<=35){
+    // alert("Obésité modérée");
+    show.textContent=`${res} you are Obésité modérée`;
+  }
 }
-setInterval(changeBgcolor,3000);
-btn.addEventListener("click",changeBgcolor);
+
+btn.addEventListener("click",calculer);
