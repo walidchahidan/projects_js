dd15296b4a4483a08292244a4ddfa456181c96ba
WALID CHAHIDANE <chahidanwalid37@gmail.com>
2022-09-12 09:48:16 +0100

    plus

---
+++ script.js
@@ -0,0 +1,9 @@
+let nbr1=document.getElementById("nbr1");
+let nbr2=document.getElementById("nbr2");
+let input=document.getElementsByTagName("input");
+let res=document.getElementById("res");
+let btn=document.getElementById("btn");
+
+btn.addEventListener("click",function(){
+  
+})
